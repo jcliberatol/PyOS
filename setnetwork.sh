dbdf740062fe47dae223c59a118fee2f95974c61
jcliberatol <jcliberatol@unal.edu.co>
2014-11-27 13:05:24 -0500

    append

--- setnetwork.sh
+++ setnetwork.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ $# = 2 ]; then
-  ifconfig wlp0s29u1u7 down
-  iw dev wlp0s29u1u7 set type ibss
-  ifconfig wlp0s29u1u7 192.160.1.1 up
-  iw dev wlp0s29u1u7 ibss join pyOSNet 2462
+  ifconfig $1 down
+  iw dev $1 set type ibss
+  ifconfig $1 $2 up
+  iw dev $1 ibss join pyOSNet 2462
 else
   echo "Por favor use setnetwork.sh deviceName IP"
 fi

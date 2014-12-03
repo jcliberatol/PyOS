46578e426fe4d168bea14890802225b849a2b8c0
jcliberatol <jcliberatol@unal.edu.co>
2014-12-02 23:20:36 -0500

    added node adding and nodes.list support

--- server.py
+++ server.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
-import os , sys , getopt, subprocess , paramiko
+import os , sys , getopt, subprocess , paramiko , csv
 
 #Things to do
-#Print the arguments
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-
+SOROOT = "/home/SOROOT"
 if len(sys.argv)>1 :
   #start network of server (args are : start)
   if str(sys.argv[1])== 'start':
     print 'starting the server'
-    SOROOT = "/home/SOROOT"
     if not os.path.isdir(SOROOT):
       os.makedirs(SOROOT)
       print "Nuevo servidor en  : " , SOROOT
@@ -28,7 +25,40 @@ if len(sys.argv)>1 :
   #add a node to the server (args are : addnode node.info)
   if str(sys.argv[1])== 'addnode':
     print 'Adding a new node'
+    host = ''
+    user = ''
+    passw = ''
+    with open('nodes/nodeexample.info', 'rb') as f:
+      reader = csv.reader(f, delimiter=' ', quoting=csv.QUOTE_NONE)
+      for row in reader:
+        host = row[0]
+        user = row[1]
+        passw = row[2]
+    print 'user is : ' , user
+    print 'host is : ' , host
+    #check if nodes file exists if not create it.
+    if not os.path.isfile(SOROOT+'/nodes.list') :
+      open((SOROOT+'/nodes.list'),'w').close()
+      print 'nodes file didnt existed, creating empty file'
+    #Now write the node to the nodes.list
+    lastID = 0
+    repeatedNode = False
+    with open((SOROOT+'/nodes.list'), 'rb') as f:
+      reader = csv.reader(f, delimiter=' ', quoting=csv.QUOTE_NONE)
+      for row in reader:
+        lastID = int(row[0])
+        if host == row[1] :
+          print 'Repeated host , not registering node'
+          repeatedNode = True
     
+    #If node is not repeated then register it as a new node
+    if not repeatedNode:  
+      with open(SOROOT+'/nodes.list', 'a') as f:
+        writer = csv.writer(f, delimiter=' ')
+        writer.writerow([lastID+1,host,user,passw])
+        print 'Registered a new node, nodeID : ', lastID+1
+    else :
+      print 'Node already registered as a node, nodeID : ' , lastID
   #executing commands
   if str(sys.argv[1])== 'exec':
     print 'executing a command'
@@ -47,3 +77,9 @@ if len(sys.argv)>1 :
     print 'Processes mode'
 else:
   print 'No arguments given, please give an argument'
+  print 'Posible Arguments : '
+  print 'start devicename IP : starts the server with this devicename and IP'
+  print 'addnode  : Add a new node with a .info file'
+  print 'exec : executes a tast or command in a nod or all the nodes or in the server'
+  print 'allprocs : watches all the proceses'
+  print 'nodePrint : list all the nodes'

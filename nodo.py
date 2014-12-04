8675c25626530042681ae0335c9f9580916c5fb8
jcliberatol <jcliberatol@unal.edu.co>
2014-12-04 10:57:28 -0500

    fixed node

--- nodo.py
+++ nodo.py
@@ -1,22 +1,33 @@
+#!/usr/bin/env python   
+
 import os
 import socket
 import getpass
+import sys
 
+#get the device name
+deviceName = str(sys.argv[1])
+#get the IP
+nodeIP = str(sys.argv[2])
+#subprocess.call(["./netdummy.sh",deviceName,serverIP])
+#Ingresar host
+host = str(sys.argv[3])
+#Ingresar password
 
-infile = open('/home/SOROOT/dir.txt','a')
-
-comando = 'hostname'
-s = os.popen(comando).read()
-
-
-print (s,' Ingrese su contrasena')
+print 'Iniciando el nodo'
+#check if node file exists if not create it.
+if not os.path.isfile('/home/SOROOT/nodo'+'.info') :
+     open(('/home/SOROOT/nodo'+host+'.info'),'w').close()
+     print 'node file didnt existed, creating empty file'
+infile = open('/home/SOROOT/nodo'+host+'.info','a')
+print (host,' Ingrese su contrasena')
 passw = getpass.getpass()
-
-infile.write(socket.gethostbyname(socket.gethostname()))
+infile.write(nodeIP)
 infile.write(" ")
-infile.write(s)
+infile.write(host)
 infile.write(" ")
 infile.write(passw)
+infile.write('\n')
 
 infile.close()
 
